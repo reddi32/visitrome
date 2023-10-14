9c5099c8acf6937784809f59a707f2c25c189086
reddi32 <rossimatteo32@gmail.com>
2023-10-14 23:42:47 +0200

    revisione di alcuni testi

--- src/app/services/contentful.service.ts
+++ src/app/services/contentful.service.ts
@@ -17,10 +17,10 @@ export class ContentfulService {
 
   getAllEntries() {
     const promise = this.client.getEntries(
-      {
+      /*{
         content_type: 'attrazioniDaVedere',
         order: 'fields.title'
-      }
+      }*/
     );
     return from(promise);
   }
