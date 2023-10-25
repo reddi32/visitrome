009d06b258c01161694bc965c2e3d8af6ff5ec80
reddi32 <rossimatteo32@gmail.com>
2023-10-25 16:28:20 +0200

    add dovemangiare

--- src/app/dovemangiare/dovemangiare.component.ts
+++ src/app/dovemangiare/dovemangiare.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ContentfulsevenService } from '../services/contentfulseven.service';
 import { Meta } from '@angular/platform-browser';
+import { FirstCard } from '../model/firstcard';
+import { Info } from '../model/info'
 
 @Component({
   selector: 'app-dovemangiare',
@@ -11,7 +13,35 @@ import { Meta } from '@angular/platform-browser';
 })
 export class DovemangiareComponent {
 
-  constructor(private contentfulsevenService: ContentfulsevenService, private route: ActivatedRoute, private meta: Meta) {}
+  listaInfo: Info[] = [];
+  listaInfo2: Info[] = [];
+  listaInfo3: Info[] = [];
+
+  card: FirstCard[] = [];
+  card2: FirstCard[] = [];
+  card3: FirstCard[] =  [];
+
+  constructor(private contentfulsevenService: ContentfulsevenService, private route: ActivatedRoute, private meta: Meta) {
+    this.listaInfo.push(
+      new Info ("spaghetti_mini.webp", "Roma è famosa per la sua cucina ricca, con specialità come la pasta cacio e pepe, la trippa alla romana e molto altro.", "Le specialità enogastronomiche di Roma", "Scopri di più")
+    );
+    this.listaInfo2.push(
+      new Info ("fioriZucca_mini.webp", "Tra le specialità si trovano le filettature di baccalà, i supplì al telefono, e la pizza fritta con vari ripieni deliziosi.", "I migliori fritti di Roma", "Scopri di più")
+    );
+    this.listaInfo3.push(
+      new Info("Carciofo_alla_Giudea_mini.webp", "I carciofi alla romana sono un'autentica prelibatezza. Assaporarli è un'esperienza gastronomica che ti avvicinerà al cuore della cucina tradizionale romana.", "I migliori carciofi alla romana", "Scopri di più")
+    );
+
+    this.card.push(
+      new FirstCard("suppli_mini.webp", "Street food a Roma", "Se sei un appassionato di street food, Roma è una città ricca di posti dove puoi assaporare autentica cucina da strada.", "Scopri di più")
+    );
+    this.card2.push(
+      new FirstCard("ristorante_mini.webp", "La nostra selezione di ristoranti", "In cerca di un ristorante a Roma? Sei nel posto giusto. Scopri la nostra selezione.", "Scopri di più")
+    );
+    this.card3.push(
+      new FirstCard("maritozzo_mini.webp", "Dove fermarsi per un dolce a Roma?", "Hai voglia di dolce? Scopri i gustosi dolci della tradizione romana.", "Scopri di più")
+    );
+  }
 
   dovemangiarePosts$ : Observable<any> | undefined;
 
